f17d9d32b17b2a1a58ecce7a7ca90b5db10b0030
tianlu17 <tianlu17@xdf.cn>
2022-11-11 18:27:40 +0800

    feat: init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,2 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({});
+export default defineNuxtConfig({
+  nitro: {
+    output: {
+      dir: '../dist',
+    },
+    devProxy: {
+      '/api': { target: 'https://example.com', changeOrigin: true },
+    },
+  },
+  vite: {
+    server: {
+      proxy: {
+        '/api': { target: 'https://example.com', changeOrigin: true },
+      },
+    },
+  },
+});
